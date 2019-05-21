d1d2392f0047433218024c4db8e56fb2bd1bafb0
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-05-22 00:55:10 +0300

    io-read-write done
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- ferris-wheel/compile-pass/io-read-write.rs
+++ ferris-wheel/compile-pass/io-read-write.rs
@@ -1,11 +1,12 @@
 // FIXME: Make me compile! Diff budget: 2 lines.
 use std::io;
+use std::io::Result;
 
 struct ReadWrapper<T: io::Read> {
     inner: T
 }
 
-impl io::Read for ReadWrapper<T> {
+impl<T: io::Read> io::Read for ReadWrapper<T> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         self.inner.read(buf)
     }
