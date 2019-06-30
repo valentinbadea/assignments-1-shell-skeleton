d21cfa074bd07e53cd27005f3941ecc5cc993ddc
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-06-30 09:36:47 +0300

    run-pass:expressions resolved
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- ferris-wheel/run-pass/expressions.rs
+++ ferris-wheel/run-pass/expressions.rs
@@ -1,6 +1,14 @@
 // FIXME: Make me pass! Diff budget: 10 lines.
 // Do not `use` any items.
 
+fn max<T: PartialOrd + PartialEq>(a: T, b: T) -> T {
+    if a>b {
+        a
+    } else {
+        b
+    }
+}
+
 // Do not change the following two lines.
 #[derive(Debug, PartialOrd, PartialEq, Clone, Copy)]
 struct IntWrapper(isize);
@@ -9,5 +17,5 @@ pub fn main() {
     assert_eq!(max(1usize, 3), 3);
     assert_eq!(max(1u8, 3), 3);
     assert_eq!(max(1u8, 3), 3);
-    assert_eq!(max(IntWrapper(120), IntWrapper(248)), IntWrapper(248));
+    assert_eq!(max(IntWrapper(120).clone(), IntWrapper(248).clone()), IntWrapper(248));
 }
