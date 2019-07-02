302fd5f59fc8529371818430a3991fa5bfd2fe48
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-07-02 21:14:55 +0300

    trait-impl: Done with 20 lines budget, instead of 25

--- ferris-wheel/run-pass/trait-impl.rs
+++ ferris-wheel/run-pass/trait-impl.rs
@@ -1,9 +1,29 @@
 // FIXME: Make me pass! Diff budget: 25 lines.
 
+#[derive(Debug)]
 enum Duration {
     MilliSeconds(u64),
     Seconds(u32),
     Minutes(u16)
+}
+
+use Duration::*;
+
+impl PartialEq<Duration> for Duration {
+    fn eq(&self, other: &Duration) -> bool {
+        let other_milli = match other {
+            &Seconds(s) => (s as u64) * 1_000u64,
+            &Minutes(min) => (min as u64) * 60_000u64,
+            &MilliSeconds(ms) => ms as u64,
+        };
+        let self_milli = match self {
+            &MilliSeconds(m) => m,
+            &Seconds(s) => (s as u64) * 1_000u64,
+            &Minutes(min) => (min as u64) * 60_000u64,
+        };
+
+        self_milli == other_milli
+    }
 }
 
 fn main() {
